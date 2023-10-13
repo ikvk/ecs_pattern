503c90b07f8800143a646ff5545b2d7b2fee1051
v.kaukin <v.kaukin@u6.ru>
2023-10-13 08:49:55 +0500

    docs

--- ecs_pattern/__init__.py
+++ ecs_pattern/__init__.py
@@ -1,5 +1,6 @@
 # Lib author: Vladimir Kaukin <KaukinVK@ya.ru>
 # Project home page: https://github.com/ikvk/ecs_pattern
+# Mirror: https://gitflic.ru/project/ikvk/ecs-pattern
 # License: Apache-2.0
 
 from .ecs import component, entity, EntityManager, System, SystemManager
